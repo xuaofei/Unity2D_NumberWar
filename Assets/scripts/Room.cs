8b51ac95d26f0be25e8094db7ba03467ca50d30c
xuaofei <zhongguohunanyiyangcangshuipu@gmail.com>
2017-02-04 08:21:56 +0800

    完善Game场景

---
+++ Assets/scripts/Room.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : KBEngine.Entity {
+
+	public override void __init__()
+	{
+
+		Debug.Log ("Room 已经创建成功！");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onTestRoom(string s){
+		Debug.LogError (s);
+	}
+}

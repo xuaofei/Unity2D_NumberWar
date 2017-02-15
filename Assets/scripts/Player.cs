04aaa23a3a7461472b4f3b0bb8288d77fe0d4482
xuaofei <yunhou@iMac.local>
2017-02-15 14:40:08 +0800

    准备进行0.2版本开发

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KBEngine;
+
+public class Player : KBEngine.Entity {
+
+	public override void __init__()
+	{
+
+		Debug.Log ("Player 已经创建成功！");
+
+		//获取大厅公告
+	}
+
+	public void DebugClientFunction(string s){
+		Debug.Log (s);
+	}
+}
